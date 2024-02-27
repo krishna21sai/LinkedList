f980d195dcd362e54636301e9e382eb551fee451
Krishnasai_21 <krishna.11.kunche@gmail.com>
2024-02-27 08:40:12 +0530

    modified program linkedlist

--- CreateLinkedList.py
+++ CreateLinkedList.py
@@ -1,1 +1,28 @@
-print("this is linked list")
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def Insert(self,data):
+        Newnode = Node(data)
+        if self.head is None:
+            self.head = Newnode
+            return
+        lnode = self.head
+        while lnode.next:
+            lnode = lnode.next
+        lnode.next = Newnode
+    def print_list(self):
+        cnode = self.head
+        while cnode:
+            print(cnode.data,end = " ")
+            cnode = cnode.next 
+
+Linkedlist  = LinkedList()
+Linkedlist.Insert(4)
+Linkedlist.Insert(0)
+Linkedlist.Insert(2)
+Linkedlist.Insert(1)
+Linkedlist.print_list()
